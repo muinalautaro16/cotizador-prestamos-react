df9b2c9087ceb1d1013ad3ed05a167f359aea9df
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2024-04-11 03:02:49 -0300

    proyecto finalizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+
+    return(
+        <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+          ¿Cuanto <span className="text-indigo-600">dinero</span> nesecitas?
+        </h1>
+    )
+}
+
+export default Header;
